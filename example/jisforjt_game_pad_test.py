5e486a36dae3febf9c8e5cce2ad776aa623ff82c
Mr. James <68863931+jisforjt@users.noreply.github.com>
2021-08-10 14:50:26 -0400

    Add files via upload

--- example/jisforjt_game_pad_test.py
+++ example/jisforjt_game_pad_test.py
@@ -10,24 +10,28 @@ while True:
     ######################################################
     #   Display Data
     ######################################################
-    clue_data[0].text = "Joystick X: {:.2f}".format(gamepad.x)
-    clue_data[1].text = "Joystick Y: {:.2f}".format(gamepad.y)
-    clue_data[2].text = "Joystick Press: {}".format(gamepad.press)
-
-    clue_data[4].text = "Button B1: {}".format(gamepad.b1)
-    clue_data[5].text = "Button B2: {}".format(gamepad.b2)
-    clue_data[6].text = "Button B3: {}".format(gamepad.b3)
-    clue_data[7].text = "Button B4: {}".format(gamepad.b4)
+    clue_data[0].text = "Joystick X Voltage: {:.2f}".format(gamepad.x_voltage)
+    clue_data[1].text = "Joystick Y Voltage: {:.2f}".format(gamepad.y_voltage)
+    clue_data[2].text = "Joystick X: {:.2f}".format(gamepad.x)
+    clue_data[3].text = "Joystick X: {:.2f}".format(gamepad.x)
+    
+    clue_data[5].text = "Joystick Press: {}".format(gamepad.press)
+    clue_data[6].text = "Button B1: {}".format(gamepad.b1)
+    clue_data[7].text = "Button B2: {}".format(gamepad.b2)
+    clue_data[8].text = "Button B3: {}".format(gamepad.b3)
+    clue_data[9].text = "Button B4: {}".format(gamepad.b4)
 
     clue_data.show()
 
     ######################################################
     #   Buzzer
     ######################################################
-    # Uncomment the code below to hear the game pad buzz on 
-    # and off every half a second. This only works if you have
-    # the physical game pad switch set to software mode.
+    # This only works if you have the physical game pad 
+    # switch set to software mode.
+    if gamepad.press == True:
+        gamepad.buzz(0.5)
+        time.sleep(0.5)
+
     
-    #gamepad.buzz(0.5)
-    #time.sleep(0.5)
+    
     
